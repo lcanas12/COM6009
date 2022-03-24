d5c1b2e34140f06df2f5d4dc7e04ef9da6f0c230
TtttttH <574173437@qq.com>
2022-03-24 09:37:11 +0000

    update

--- ecolab/__init__.py
+++ ecolab/__init__.py
@@ -1,8 +1,10 @@
 import numpy as np
+import numba
 import matplotlib
-from ecolab.agents import Rabbit, RHD_Status
+from ecolab.agents import Rabbit, RHD_Status, Gender, AgentType
 import matplotlib.pyplot as plt
 
+@numba.jit  
 def run_ecolab(env, agents, Niteration=360, earlystop=True):
 
     record=[] #TODO
@@ -11,22 +13,23 @@ def run_ecolab(env, agents, Niteration=360, earlystop=True):
         month = (it / 30) % 12  ## 0~11 ->  Jan~Dec
         for agent in agents:
             agent.other_daily_grow(agents)
-            if month > 3:
+            if month > 3 and agent.type == AgentType.Adults and agent.gender == Gender.Female and agent.pregnancy_days == -1:
                 agent.reproduct(agents)
             agent.move(env)
             agent.die()
             
         agents = [a for a in agents if a.death == False]
         for agent in agents:
-            agent.infection(agents)
+            if agent.type == AgentType.Adults and agent.rhd_status == RHD_Status.Susceptible:
+                agent.infection(agents)
             newborn = agent.born_new_rabbit()
             if newborn is not None:
                 agent += newborn
         
         
         record.append({'susceptible agents': np.array([a.summary_vector() for a in agents if a.rhd_status == RHD_Status.Susceptible]),
-                      'infected agents': np.array(a.summary_vector() for a in agents if a.rhd_status == RHD_Status.Infected),
-                      'immune agents': np.array(a.summary_vector() for a in agents if a.rhd_status == RHD_Status.Recoverd_Immune)})        
+                      'infected agents': np.array([a.summary_vector() for a in agents if a.rhd_status == RHD_Status.Infected]),
+                      'immune agents': np.array([a.summary_vector() for a in agents if a.rhd_status == RHD_Status.Recoverd_Immune])})        
         
                 
         if earlystop:
