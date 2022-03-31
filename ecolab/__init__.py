57a5ab553c144c840b8cf84b9d0d9c601de184f1
lcanas12 <83920134+lcanas12@users.noreply.github.com>
2022-03-31 13:13:21 +0100

    Add files via upload

--- ecolab/__init__.py
+++ ecolab/__init__.py
@@ -4,40 +4,57 @@ import matplotlib
 from ecolab.agents import Rabbit, RHD_Status, Gender, AgentType
 import matplotlib.pyplot as plt
 
-@numba.jit  
-def run_ecolab(env, agents, Niteration=360, earlystop=True):
 
-    record=[] #TODO
+@numba.jit  
+def run_ecolab(env, agents, Niteration=[0, 365], max_density = 20, earlystop=True):
+
+    #record=[] 
     sus = []
     infected = []
     immune = []
-    #male = []
-    #female = []
     infant = []
     total =[]
-    days=[]
-    for it in range(Niteration):
-        print("iteration: %g" %it)
+    
+    preg_prob = {11: 11/365,
+                 0: 35/365,
+                 1: 59/365,
+                 2: 82/365,
+                 3: 59/365,
+                 4: 35/365,
+                 5: 11/365,
+                 6: 5/365,
+                 7: 5/365,
+                 8: 5/365,
+                 9: 5/365,
+                 10: 5/365}
+    for it in range(Niteration[0], Niteration[1], 1):
+        #print("iteration: %g" %it)
         month = (it / 30) % 12  ## 0~11 ->  Jan~Dec
+        prob = preg_prob.get(int(month))
         for agent in agents:
             agent.other_daily_grow()
-            if month > 3 and agent.type == AgentType.Adults and agent.gender == Gender.Female and agent.pregnancy_days == -1:
-                agent.reproduct(agents)
-            agent.move(env)
-            agent.die()
+            if not agent.death:
+                if agent.type == AgentType.Adults and agent.gender == Gender.Female and agent.pregnancy_days == -1:
+                    agent.reproduct(agents,prob=prob)
+                agent.move(env)
+                agent.die()
             
-        agents = [a for a in agents if a.death == False]
-        for agent in agents:
+        alive_agents = [a for a in agents if not a.death]
+        death_in_90_days_agents = [a for a in agents if a.death and a.days_dead < 90]
+        for agent in alive_agents:
             if agent.rhd_status == RHD_Status.Infected and agent.infected_days > 0:
-                agent.infection(agents)
-            newborn = agent.born_new_rabbit(agents, env)
+                agent.infection(alive_agents)
+            
+            if agent.type == AgentType.Adults and agent.rhd_status == RHD_Status.Susceptible:
+                agent.carcasses_infection(death_in_90_days_agents)
+            newborn = agent.born_new_rabbit(agents, env, max_density)
             if newborn is not None:
                 agents += newborn
         
-        
-        record.append({'susceptible agents': np.array([a.summary_vector() for a in agents if a.rhd_status == RHD_Status.Susceptible]),
-                      'infected agents': np.array([a.summary_vector() for a in agents if a.rhd_status == RHD_Status.Infected]),
-                      'immune agents': np.array([a.summary_vector() for a in agents if a.rhd_status == RHD_Status.Recoverd_Immune])})   
+        alive_agents = [a for a in agents if not a.death]
+        #record.append({'susceptible agents': np.array([a.summary_vector() for a in alive_agents if a.type ==AgentType.Adults and a.rhd_status == RHD_Status.Susceptible]),
+        #               'infected agents': np.array([a.summary_vector() for a in alive_agents if a.rhd_status == RHD_Status.Infected]),
+        #              'immune agents': np.array([a.summary_vector() for a in alive_agents if a.rhd_status == RHD_Status.Recoverd_Immune])})   
         
         #print("the number of whole agents: ", len(agents))
         #print("the number of Female adults", len([a for a in agents if a.type == AgentType.Adults and a.gender == Gender.Female]))
@@ -48,16 +65,13 @@ def run_ecolab(env, agents, Niteration=360, earlystop=True):
         #print("the number of infected agents: ", len(record[it]['infected agents']))
         #print("the number of immune agents:", len(record[it]['immune agents']))
         #print("===========================================")    
-        days.append(it)
-        sus.append(len(record[it]['susceptible agents']))
-        infected.append(len(record[it]['infected agents']))
-        immune.append(len(record[it]['immune agents']))
-        total.append(len(agents))
-        infant.append(len([a for a in agents if a.type == AgentType.Infants]))                
+        sus.append(len([a for a in alive_agents if a.type == AgentType.Adults and a.rhd_status == RHD_Status.Susceptible]))
+        infected.append(len([a for a in alive_agents if a.rhd_status == RHD_Status.Infected]))
+        immune.append(len([a for a in alive_agents if a.rhd_status == RHD_Status.Recoverd_Immune]))
+        total.append(len(alive_agents))
+        infant.append(len([a for a in alive_agents if a.type == AgentType.Infants]))                 
         
         if earlystop:
             if len(agents)==0: break
     
-    return record, sus, infected, immune, total, infant       
-
-    
+    return sus, infected, immune, total, infant, agents
--- ecolab/agents.py
+++ ecolab/agents.py
@@ -10,9 +10,8 @@ import random
 
 INFANT_NATURE_MOTALITY_RATE = 0.0002
 ADULT_NATURE_MOTALITY_RATE = 0.0013
-#RHD_INFECTION_PROB = 1.27 * 10e-4
-#PREGNANT_PROB = 0.4
-MAX_CAPCITY = 40 # max capcity for each grid
+CARCASS_INFECTION_PROB = 0.5
+
 class RHD_Status(Enum):
     """
     RHD_Status
@@ -39,12 +38,14 @@ class Rabbit:
     """
     Base class for all types of rabbit
     """
-    pregnancy_days = -1
-    infected_days = -1
+
     maxage = 3650
     def __init__(self,position,age,infected=False):
         self.position = position
         self.age = age
+        self.pregnancy_days = -1
+        self.infected_days = -1
+        self.days_dead = -1
         if np.random.randint(0,2) == 1: ## 1 is male, 0 is female
             self.gender = Gender.Male
         else:
@@ -54,7 +55,7 @@ class Rabbit:
             self.rhd_status = RHD_Status.Susceptible
         else :
             self.rhd_status = RHD_Status.Infected
-            self.infected_days = 1
+            self.infected_days = 0
         if age < 90:
             self.speed = 0
             self.type = AgentType.Infants
@@ -81,55 +82,54 @@ class Rabbit:
         if self.rhd_status == RHD_Status.Infected and self.infected_days > 0: ##infected death begins on the 2nd infected day
             # self.death = (np.random.rand() > 0.1 * self.infected_days)
             self.death = (np.random.rand() < np.exp(-self.infected_days))
-        if self.death == True: return 
+            
         # nature death
-        if self.age > self.maxage: 
+        if self.age > self.maxage and not self.death: 
             self.death = True
-            return 
-        if self.type == AgentType.Infants:
-            self.death =  (np.random.rand() < INFANT_NATURE_MOTALITY_RATE)
-        else:
-            self.death = (np.random.rand() < ADULT_NATURE_MOTALITY_RATE)
     
-    @numba.jit         
-    def get_nearby_rabbit(self, position, agents):
-        return [a for a in agents if (np.abs(a.position[0] - position[0]) < 2 and np.abs(a.position[1] - position[1]) < 2)]
+        if not self.death:
+            if self.type == AgentType.Infants:
+                self.death =  (np.random.rand() < INFANT_NATURE_MOTALITY_RATE)
+            
+            else:
+                self.death = (np.random.rand() < ADULT_NATURE_MOTALITY_RATE)
+                
+        if self.death:
+            self.days_dead = 0
     
     @numba.jit 
-    def infection(self, agents):
+    def infection(self, alive_agents):
         cnt = 0
-        for a in agents:
+        for a in alive_agents:
             if (a.type == AgentType.Adults and a.rhd_status == RHD_Status.Susceptible and (a.position == self.position).all()):
-                print("one rabbbit get infected")
+                #print("one rabbbit get infected")
                 a.infected_days = 0
                 a.rhd_status = RHD_Status.Infected
                 cnt += 1
-            if cnt == 2: break
-        # nearby_susceptible_agents = [a for a in agents if (a.rhd_status == RHD_Status.Susceptible and (a.position == self.position).all())]
-        # # nearby_infected_agents = [a for a in agents if a.rhd_status == RHD_Status.Infected and a.infected_days > 0 and np.abs(a.position[0] - self.position[0]) < 2 and np.abs(a.position[1] - self.position[1]) < 2]
-        # if len(nearby_susceptible_agents) > 0:
-        #     chosen_agent = random.sample(nearby_susceptible_agents, 2)
-        #     for a in chosen_agent:
-        #         a.infected_days = 0   ## initial day of infection
-        #         a.rhd_status = RHD_Status.Infected
-            
-            # print("one rabbit get infected")
-            #np.abs(a.position[0] - self.position[0]) < 2 and np.abs(a.position[1] - self.position[1]) < 2)
+            if cnt == 2: break          
+             
+    @numba.jit 
+    def carcasses_infection(self, death_in_90_days_agents):
+        nearby_dead_agents = [a for a in death_in_90_days_agents if ((a.position == self.position).all())]
+        if len(nearby_dead_agents) > 0 and np.random.rand() <= CARCASS_INFECTION_PROB:
+            #print("there was a dead rabbit")
+            self.infected_days = 0 
+            self.rhd_status = RHD_Status.Infected
 
-    # @numba.jit 
-    def reproduct(self, agents):
-        same_grid_male = [a for a in agents if (a.death == False and a.type == AgentType.Adults and a.gender == Gender.Male and (a.position == self.position).all())]
-        prob = np.random.randint(11,83) / 100
+    @numba.jit 
+    def reproduct(self, agents, prob):
+        same_grid_male = [a for a in agents if (not a.death and a.type == AgentType.Adults and a.gender == Gender.Male and (a.position == self.position).all())]
         if len(same_grid_male) > 0 and np.random.rand() <= prob:
             self.pregnancy_days = 0
             # print("one female adult rabbit get pregant")
     
     
     @numba.jit             
-    def born_new_rabbit(self,agents,env):
+    def born_new_rabbit(self,agents,env,max_density):
         if self.pregnancy_days > 30:
             self.pregnancy_days = -1
-            if len(agents) / env.shape[0] < MAX_CAPCITY:
+            alive_num = len([a for a in agents if not a.death])
+            if alive_num / (env.shape[0] * env.shape[1]) < max_density:
                 litter_num = np.random.randint(2, 8)
                 newborn =[]
                 i = 0
@@ -143,22 +143,24 @@ class Rabbit:
             
     @numba.jit 
     def other_daily_grow(self): # run this function first in each day loop
-        self.age +=1
-        if self.pregnancy_days > -1:
-            self.pregnancy_days += 1
+        if self.death == True:
+            self.days_dead += 1
+        else:
+            self.age +=1
+            if self.pregnancy_days > -1:
+                self.pregnancy_days += 1
         
-        if self.rhd_status == RHD_Status.Infected:
-            self.infected_days += 1
-        
-        if self.infected_days > 10:
+            if self.rhd_status == RHD_Status.Infected:
+                self.infected_days += 1
+
+            if self.infected_days > 10:
             # print("here is a new immune rabbit!")
-            self.rhd_status = RHD_Status.Recoverd_Immune
-            self.infected_days = -1
+                self.rhd_status = RHD_Status.Recoverd_Immune
+                self.infected_days = -1    
             
-            
-        if self.age > 90:
-            self.speed = 5
-            self.type = AgentType.Adults
+            if self.age > 90:
+                self.speed = 5
+                self.type = AgentType.Adults
             
     @numba.jit        
     def summary_vector(self):
